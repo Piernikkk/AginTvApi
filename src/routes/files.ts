547cbdf90525163eed99bdd353d5b5f16cbe4547
Michał Ziernik <ziernikmichal@gmail.com>
2025-01-28 12:07:10 +0100

    No file error

--- src/routes/files.ts
+++ src/routes/files.ts
@@ -4,6 +4,11 @@ import upload from '../functions/uploader';
 const files = express.Router({ mergeParams: true });
 
 files.post('/upload', upload.single("file"), async (req, res) => {
+    if (!req?.file) {
+        res.status(400).json({ error: 'No file uploaded' });
+        return;
+    }
+
     const fileData = {
         path: req?.file?.path,
         originalName: req?.file?.originalname,
